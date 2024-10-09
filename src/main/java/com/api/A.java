23b452411a0e694742bf94ae2fe54694bca8fc8d
sanafatmagit <sanafatma2022@gmail.com>
2024-10-09 14:15:28 +0530

    First V1

---
+++ src/main/java/com/api/A.java
@@ -0,0 +1,14 @@
+package com.api;
+
+import java.util.function.Predicate;
+
+public class A {
+    public static void main(String[] args) {
+     A a1 = new A();
+     a1.test();
+
+    }
+    public void test(){
+        System.out.println("Hello");
+    }
+}
